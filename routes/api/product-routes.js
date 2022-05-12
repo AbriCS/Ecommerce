7c86fd5c605d01924f7153c8503b55adfe68feac
Jean McLeod <abricsltd@gmail.com>
2022-05-12 13:06:48 +0100

    add Product-routes delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -111,6 +111,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+   where:{id:req.params.id} 
+  })
+  .then((deleteProduct)=>{
+    res.status(200).json(deleteProduct)
+  }).catch ((err)=>{
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
